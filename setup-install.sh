13a8eb9d82a12a10c9b14635eb499230783f4184
darknight-007 <jnaneshwar.das@gmail.com>
2016-02-08 03:32:14 -0500

    trying to make the setup non-interactive, but gazebo install script has interactive apt-get

--- setup-install.sh
+++ setup-install.sh
@@ -21,10 +21,6 @@ wget -O /tmp/gazebo6_install.sh http://osrf-distributions.s3.amazonaws.com/gazeb
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 sudo apt-get update
-sudo apt-get install ros-indigo-desktop
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-sudo apt-get update
 sudo apt-get -q -y install ros-indigo-desktop
 sudo rosdep init
 rosdep update
